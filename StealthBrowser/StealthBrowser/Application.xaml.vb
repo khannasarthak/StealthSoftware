d5ff69c46b31a4b741b81ea06ab1ff97000c6e39
khannasarthak <khannasarthak.1994@gmail.com>
2015-01-12 21:43:30 +0530

    Windows App
    Basic Windows app

---
+++ StealthBrowser/StealthBrowser/Application.xaml.vb
@@ -0,0 +1,38 @@
+﻿
+Class Application
+    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+    ' can be handled in this file.
+    private WebView webView;
+
+    Public WebForm()
+{
+
+    InitializeComponent();
+
+    // Create a windowed WebView instance matching
+    // the size of the container.
+    webView = WebCore.CreateWebView( 
+        this.ClientSize.Width, 
+        this.ClientSize.Height, 
+        ViewType.Window );
+}
+
+protected override void OnHandleCreated(EventArgs e)
+{
+	base.OnHandleCreated(e);
+
+	if (webView == null)
+	    return;
+
+	// Before using a windowed WebView, we need
+	// to assign a parent window.    
+	webView.ParentWindow = this.Handle;
+}
+    webView.Source = new Uri( "http://www.google.com" );
+End Class
+
+
+
+
+
+

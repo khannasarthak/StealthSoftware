209c15ce44bf327d1fa701aa21eaf7687cc30629
khannasarthak <khannasarthak.1994@gmail.com>
2015-01-13 22:55:27 +0530

    Made sample Admin Page
    Still testing

---
+++ webapp/admin.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel=stylesheet href="css/materialize.css" media="screen,projection">
+
+    <link type="text/css" rel="stylesheet" href="css/animate.css">
+    <link type="text/css" rel="stylesheet" href="css/styleadmin.css">
+	
+        <!--<script type="text/javascript" src="js/init.js"></script>-->
+        <script src="js/jquery.min.js"></script>
+<script src="js/jquery.ui.shake.js"></script>
+    <script src="js/adminjquery.js"></script>
+
+   
+    <title>Administrator Login</title>
+</head>
+    <body class="blue">  
+        <div class="container">
+           <div class="row">
+                <div class="col s3"  class="z-depth-1" id="topbarleft">1</div>
+                <div class="col s6"  class="z-depth-3" id="topbarmid">2</div>
+                <div class="col s3"  class="z-depth-5" id="topbarright">3</div>
+                   
+            </div>
+            <div class="row">
+      <div class="col s12">
+        <ul class="tabs">
+          <li class="tab col s3"><a href="#test1">Users Online</a></li>
+          <li class="tab col s3"><a class="active" href="#test2">Admin Details</a></li>
+          <li class="tab col s3"><a href="#test3">Billing Details</a></li>
+          <li class="tab col s3"><a href="#test4">Message</a></li>
+        
+        </ul>
+      </div>
+      <div id="test1" class="col s12">
+        
+      </div>
+      <div id="test2" class="col s12">
+          <ul class="collapsible">
+              <li>
+                  <div class="collapsible-header"><i class="mdi-image-filter-drama"></i>Add/Remove New User</div>
+                  <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+              </li>
+              <li>
+                  <div class="collapsible-header"><i class="mdi-maps-place"></i>See Logs</div>
+                  <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                  </li>
+              <li>
+                  <div class="collapsible-header"><i class="mdi-social-whatshot"></i>Third</div>
+                  <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+              </li>
+          </ul>
+      </div>
+    <div id="test3" class="col s12"></div>
+    <div id="test4" class="col s12"></div>
+  </div>
+        </div> 
+        
+
+          
+                
+                
+       <!-- <script type="text/javascript" src="js/prism.js"></script>-->
+        <script type="text/javascript" src="js/materialize.js"></script>
+    </body>
+    
+    
+
+</html>
---
+++ webapp/js/adminjquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+            $('ul.tabs').tabs();
+          });
+$(document).ready(function(){
+        $('.collapsible').collapsible();
+      });
+        
